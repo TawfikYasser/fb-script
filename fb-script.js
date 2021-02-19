bc7401ae312dd54f0a0f70568ca7f1fe3ba82c9b
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-02-19 17:51:46 +0200

    Update fb-script.js

--- fb-script.js
+++ fb-script.js
@@ -1,10 +1,7 @@
-/**
- * 
+/** 
  * The following JavaScript Code to delete/accept all friend requests in one second.
- * 
+ * حذف او قبول | Accept or Delete
  */
-
-const type = "حذف"; // type => حذف او قبول | Accept or Delete
-document.querySelectorAll(`[aria-label^=${type}]`)
-var deletedList = [...document.querySelectorAll(`[aria-label^=${type}]`)];
+document.querySelectorAll('[aria-label^="حذف"]');
+var deletedList = [...document.querySelectorAll('[aria-label^="حذف"]')];
 deletedList.forEach(card => card.click());
